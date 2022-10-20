169aa5d9f3d738a6637e2040e7acee4d634dd24d
Nurul Ainun <nurulainun310898@gmail.com>
2022-10-20 19:12:34 +0800

    initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+const server = Hapi.server({
+  port: 5000,
+  host: 'localhost',
+  routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+});
+
+console.log(server);
+
+init();
